2ab580cebb43840ea76e81812cae0bc5bf8caf70
Nathanael DCunha <nathanael.dcunha@gmail.com>
2024-03-20 00:11:30 +0530

    chore: added stories section to content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Story } from "@/components/story";
 import { TabContent, TabItem, Tabs } from "@/components/tabs";
 
 export default function Page() {
@@ -7,16 +8,29 @@ export default function Page() {
 				<TabItem index="discussion">Discussion Forum</TabItem>
 				<TabItem index="market">Market Stories</TabItem>
 			</div>
-			<div className="max-w-[1100px] w-full mx-auto flex">
-				<TabContent index="discussion" className="flex-[7]">
-					<h2 className="hidden md:block text-2xl text-red font-semibold uppercase">
-						Discussion Forum
-					</h2>
+			<div className="wrapper">
+				<TabContent index="discussion" className="p-5 md:p-0 flex-[7]">
+					<h2 className="section-title">Discussion Forum</h2>
 				</TabContent>
-				<TabContent index="market" className="flex-[3]">
-					<h2 className="hidden md:block text-2xl text-red font-semibold uppercase">
-						Market Stories
-					</h2>
+				<TabContent index="market" className="p-5 md:p-0 flex-[3]">
+					<h2 className="section-title">Market Stories</h2>
+					<div className="space-y-4">
+						<Story
+							title="The Coldest Sunset"
+							content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+							image="https://img.freepik.com/free-vector/gradient-mountain-landscape_23-2149152830.jpg"
+						/>
+						<Story
+							title="The Coldest Sunset"
+							content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+							image="https://img.freepik.com/free-vector/gradient-mountain-landscape_23-2149152830.jpg"
+						/>
+						<Story
+							title="The Coldest Sunset"
+							content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+							image="https://img.freepik.com/free-vector/gradient-mountain-landscape_23-2149152830.jpg"
+						/>
+					</div>
 				</TabContent>
 			</div>
 		</Tabs>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
 			},
 			red: "#ef4444",
 			gray: "#6b7280",
+			offwhite: "#d2d2d2",
 		},
 		extend: {},
 	},
