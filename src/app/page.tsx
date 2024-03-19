c3b9e984333f37cb32a26e0d0bb3bdbfb2c51444
Nathanael DCunha <nathanael.dcunha@gmail.com>
2024-03-19 19:34:46 +0530

    chore: added main content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import { TabContent, TabItem, Tabs } from "@/components/tabs";
+
 export default function Page() {
-	return <div className="max-w-[1100px] w-full h-full mx-auto flex"></div>;
+	return (
+		<div className="w-full flex flex-col">
+			<Tabs defaultIndex="discussion">
+				<div className="flex md:hidden">
+					<TabItem index="discussion">Discussion Forum</TabItem>
+					<TabItem index="market">Market Stories</TabItem>
+				</div>
+				<div className="max-w-[1100px] w-full mx-auto flex">
+					<TabContent index="discussion" className="flex-[7]">
+						<h2 className="hidden md:block text-2xl text-red font-semibold uppercase">
+							Discussion Forum
+						</h2>
+					</TabContent>
+					<TabContent index="market" className="flex-[3]">
+						<h2 className="hidden md:block text-2xl text-red font-semibold uppercase">
+							Market Stories
+						</h2>
+					</TabContent>
+				</div>
+			</Tabs>
+		</div>
+	);
 }
