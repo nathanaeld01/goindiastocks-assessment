fd42ccb8841815a846ba4599c23ed218e5bf85be
Nathanael DCunha <nathanael.dcunha@gmail.com>
2024-03-20 16:00:49 +0530

    chore: added post details and content sections

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,19 +1,67 @@
+import "@/styles/post.css";
+
+import { formatNumber } from "@/lib/utils";
+
+import {
+	EyeIcon,
+	HeartIcon,
+	MessageSquareIcon,
+	Share2Icon,
+} from "lucide-react";
+
 type Props = {
+	content: string;
 	author: string;
+	likes?: number;
+	views?: number;
+	comments?: number;
 };
 
-export const Post = ({ author }: Props) => {
+export const Post = ({ content, author, likes, views, comments }: Props) => {
 	return (
-		<div className="w-full flex border border-offwhite rounded-md shadow-[3px_3px_7px_0px] shadow-black/10">
-			<div className="w-14 h-full p-2.5 shrink-0">
+		<div className="post-item">
+			<div className="post-header">
 				<div className="relative size-9 bg-primary/10 rounded-full"></div>
 			</div>
-			<div className="w-full h-full grid grid-rows-[1fr_auto_1fr] *:w-full">
-				<div className="h-8 flex items-center">
-					<span className="font-semibold text-gray">{author}</span>
+			<div className="post-content">
+				<div className="flex py-2 items-center">
+					<span className="text-sm font-semibold text-gray">
+						{author}
+					</span>
+					<span className="ml-8 block px-2.5 py-0.5 bg-lightblue text-white text-[0.675rem] rounded-full">
+						Sector 2
+					</span>
+					<span className="ml-auto text-[.675rem] text-lightblue font-semibold">
+						2 min ago
+					</span>
 				</div>
-				<div className=""></div>
-				<div className="h-10 flex *:flex-1"></div>
+				<div className="text-sm text-gray">{content}</div>
+				<div className="h-9 flex gap-2 justify-stretch">
+					<div className="post-metric">
+						<HeartIcon className="size-5" />
+						<span className="flex font-semibold text-xs">
+							{formatNumber(likes ?? 0)}
+						</span>
+					</div>
+					<div className="post-metric">
+						<EyeIcon className="size-5" />
+						<span className="flex font-semibold text-xs">
+							{formatNumber(views ?? 0)}
+						</span>
+					</div>
+					<div className="post-metric">
+						<MessageSquareIcon className="size-5" />
+						<span className="flex font-semibold text-xs">{`${formatNumber(
+							comments ?? 0,
+						)} Comments`}</span>
+					</div>
+					<div className="post-metric">
+						<Share2Icon className="size-5" />
+						<span className="flex font-semibold text-xs">
+							Share
+						</span>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
