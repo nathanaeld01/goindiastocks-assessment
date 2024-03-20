63dd32d0c3a78874ca0c0f8bb11cdc419ece7a08
Nathanael DCunha <nathanael.dcunha@gmail.com>
2024-03-20 16:01:28 +0530

    style: added image styles

--- src/components/story.tsx
+++ src/components/story.tsx
@@ -9,8 +9,13 @@ type Props = {
 export const Story = ({ title, image, content }: Props) => {
 	return (
 		<div className="flex flex-col border border-offwhite rounded-md overflow-hidden">
-			<div className="relative h-28 aspect-video">
-				<Image src={image} alt={title} fill className="z-0" />
+			<div className="relative h-32 aspect-video">
+				<Image
+					src={image}
+					alt={title}
+					fill
+					className="z-0 object-cover"
+				/>
 			</div>
 			<div className="flex p-2.5 flex-col">
 				<h5 className="text-base font-semibold">{title}</h5>
