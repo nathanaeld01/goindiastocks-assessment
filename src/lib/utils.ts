bdc9f404deb6f97e6d0e27631963d0e4c9809796
Nathanael DCunha <nathanael.dcunha@gmail.com>
2024-03-21 21:13:39 +0530

    fix: remove decimals for leading zero numbers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,11 +6,15 @@ function cn(...input: ClassValue[]) {
 	return twMerge(clsx(input));
 }
 
-function formatNumber(num: number, precision?: number) {
-	if (num >= 1e12) return `${(num / 1e12).toFixed(precision ?? 0)}T`;
-	if (num >= 1e9) return `${(num / 1e9).toFixed(precision ?? 0)}B`;
-	if (num >= 1e6) return `${(num / 1e6).toFixed(precision ?? 0)}M`;
-	if (num >= 1e3) return `${(num / 1e3).toFixed(precision ?? 0)}K`;
+function formatNumber(num: number, precision = 1) {
+	if (num >= 1e12)
+		return `${(num / 1e12).toFixed(precision).replace(/\.0$/, "")}T`;
+	if (num >= 1e9)
+		return `${(num / 1e9).toFixed(precision).replace(/\.0$/, "")}B`;
+	if (num >= 1e6)
+		return `${(num / 1e6).toFixed(precision).replace(/\.0$/, "")}M`;
+	if (num >= 1e3)
+		return `${(num / 1e3).toFixed(precision).replace(/\.0$/, "")}K`;
 	return num;
 }
 
